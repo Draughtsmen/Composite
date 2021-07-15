142c37ca8dc408d368be6e50bccf652dd847b3dc
DefinitiveAbove <definitiveabove@gmail.com>
2021-07-14 20:14:42 -0400

    Fix linting errors

--- src/app/composite-class.spec.ts
+++ src/app/composite-class.spec.ts
@@ -2,6 +2,6 @@ import { CompositeClass } from './composite-class';
 
 describe('CompositeClass', () => {
   it('should create an instance', () => {
-    expect(new CompositeClass("test")).toBeTruthy();
+    expect(new CompositeClass('test')).toBeTruthy();
   });
 });
