2ab2bb6e3de243f8fcdf10b659afe6de253fea23
samiamiamsamiam <samuelrobertstuart@gmail.com>
2021-07-15 20:03:21 -0400

    Commented out tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,16 @@ export class AppComponent {
   title = 'composite';
 
   constructor() {
-    let test = new CompositeClass('testname');
-    let inpjson = '{"types": [{"type": "string","format": "var $1 = $2;"},{"type": "real","format": "var $1 = $2;"},{"type": "array","format": "<...>"}],"templates": [{"name": "function","format": "<...>"},{"name": "script","format": "<...>"},{"name": "object","format": "<...>"}],"singleCommentRule": "//$1","multiCommentRule": "/*n$1*/"}';
-    console.log("Langtest types");
+    // Some tests, commented out for now (remove eventually)
+    //let test = new CompositeClass('testname');
+    //let inpjson = '{"types": [{"type": "string","format": "var $1 = $2;"},{"type": "real","format": "var $1 = $2;"},{"type": "array","format": "<...>"}],"templates": [{"name": "function","format": "<...>"},{"name": "script","format": "<...>"},{"name": "object","format": "<...>"}],"singleCommentRule": "//$1","multiCommentRule": "/*n$1*/"}';
+    /*console.log("Langtest types");
     let langtest : LanguageSupportFormat = JSON.parse(inpjson);
     langtest.types.forEach(element => {
       console.log(element.type + ": " + element.format)
     });
     
     console.log("Langtest templates: "+ langtest.templates);
-    console.log("Langtest single com rule: "+ langtest.singleCommentRule);
+    console.log("Langtest single com rule: "+ langtest.singleCommentRule);*/
   }
 }
