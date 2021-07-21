e13431870ea44629ab137376e20f79e1f85ee01e
i-am-david-liu <69320783+i-am-david-liu@users.noreply.github.com>
2021-07-20 20:11:41 -0400

    Formatting

--- src/app/classes/document-support-format.ts
+++ src/app/classes/document-support-format.ts
@@ -1,12 +1,12 @@
 // An interface to fetch documentation formatting stored in JSON files
 export interface DocumentSupportFormat {
-    specs: Array<spec>;
-    container: Array<string>;
+  specs: Array<spec>;
+  container: Array<string>;
 }
   
-  // A helper interface to store specs
-  interface spec {
-    type: string;
-    format: string;
-  }
+// A helper interface to store specs
+interface spec {
+  type: string;
+  format: string;
+}
   
