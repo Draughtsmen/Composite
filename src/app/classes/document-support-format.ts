1c4976360c3210da3f6105578af1535097fa7371
i-am-david-liu <69320783+i-am-david-liu@users.noreply.github.com>
2021-07-23 15:01:42 -0400

    Doc support format (#10)
    * Create docs, langs folders under conf
    
    docs -> documentation configs
    lang -> language configs
    
    * Add javadocs.json configuration file
    
    * Add document-support-format.ts
    
    * Add gmldocs.json configuration file
    
    * Formatting
    
    * Formatting
    
    * Change param to parameter in gmldocs.json

---
+++ src/app/classes/document-support-format.ts
@@ -0,0 +1,11 @@
+// An interface to fetch documentation formatting stored in JSON files
+export interface DocumentSupportFormat {
+  specs: Array<spec>;
+  container: Array<string>;
+}
+
+// A helper interface to store specs
+interface spec {
+  type: string;
+  format: string;
+}
