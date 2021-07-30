13846395746e8924719d9293ddf451dc73463d13
samiamiamsamiam <samuelrobertstuart@gmail.com>
2021-07-30 17:30:10 -0400

    linted

--- test/spec.js
+++ test/spec.js
@@ -1,10 +1,10 @@
-const Application = require('spectron').Application
-const assert = require('assert')
-const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
-const path = require('path')
+const Application = require("spectron").Application;
+const assert = require("assert");
+const electronPath = require("electron"); // Require Electron from the binaries included in node_modules.
+const path = require("path");
 
-describe('Application launch', function () {
-  this.timeout(10000)
+describe("Application launch", function () {
+  this.timeout(10000);
 
   beforeEach(function () {
     this.app = new Application({
@@ -26,23 +26,23 @@ describe('Application launch', function () {
 
       // The following line tells spectron to look and use the main.js file
       // and the package.json located 1 level above.
-      args: [path.join(__dirname, '..')]
-    })
-    return this.app.start()
-  })
+      args: [path.join(__dirname, "..")],
+    });
+    return this.app.start();
+  });
 
   afterEach(function () {
     if (this.app && this.app.isRunning()) {
-      return this.app.stop()
+      return this.app.stop();
     }
-  })
+  });
 
-  it('shows an initial window', function () {
+  it("shows an initial window", function () {
     return this.app.client.getWindowCount().then(function (count) {
-      assert(count == 1|| count == 2)
+      assert(count == 1 || count == 2);
       //assert.equal(count, 1)
       // Please note that getWindowCount() will return 2 if `dev tools` are opened.
       //assert.equal(count, 2)
-    })
-  })
-})
+    });
+  });
+});
