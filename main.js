87c3b61ef4dc37308e2824fd2e7b1673f3cdf0a2
samiamiamsamiam <samuelrobertstuart@gmail.com>
2021-08-06 13:08:18 -0400

    trying some preload shennanigans

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
+      preload: "preload.ts",
       nodeIntegration: true,
       contextIsolation: false,
     },
