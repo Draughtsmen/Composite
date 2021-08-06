85f79f0837cdfc01653fe931f87632316e1ae3cf
samiamiamsamiam <samuelrobertstuart@gmail.com>
2021-08-06 14:53:53 -0400

    maybe removing sandbox mode works?

--- test/spec.js
+++ test/spec.js
@@ -26,7 +26,7 @@ describe("Application launch", function () {
 
       // The following line tells spectron to look and use the main.js file
       // and the package.json located 1 level above.
-      args: [path.join(__dirname, "..")],
+      args: ["--no-sandbox", path.join(__dirname, "..")],
     });
     return this.app.start();
   });
