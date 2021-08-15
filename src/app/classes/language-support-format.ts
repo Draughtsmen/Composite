8daa821709e8532f5f65e440575208642c84f3bf
samiamiamsamiam <samuelrobertstuart@gmail.com>
2021-08-15 03:54:52 -0400

    Project exports now work
    Also made the naming of LanguageSupportFormat reflect the JSON changes (type is now id)

--- src/app/classes/language-support-format.ts
+++ src/app/classes/language-support-format.ts
@@ -11,7 +11,7 @@ export interface LanguageSupportFormat {
 
 interface project {
   name: string;
-  type: string;
+  id: string;
   append: string;
 }
 
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,10 +6,9 @@ import {
   FormGroup,
 } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { dialog } from 'electron';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { sandboxed } from 'process';
-
 import { Composite } from '../classes/composite';
 import { CompositeClass } from '../classes/composite-class';
 import { CompositeFunction } from '../classes/composite-function';
@@ -19,6 +18,7 @@ import { CompositeVariable } from '../classes/composite-variable';
 import { IpcService } from '../ipc.service';
 import { CompositeManagerService } from '../services/composite-export/composite-manager.service';
 import { FormsModule } from '@angular/forms';
+import { fileURLToPath } from 'url';
 
 /**
  * This class describes the main component of the frontend.
@@ -294,11 +294,22 @@ export class MainComponent {
     }
     this.saveComposite();
   }
+
   /**
    * Saves the current project to files
    */
-  saveToFiles()
-  {
-    
+  saveToFiles() {
+    let proj = Array<any>();
+    this.project?.files.forEach((e) => {
+      if (this.project)
+        proj.push({
+          name: e.getName(),
+          data: e.generateStub(this.project.lang, this.project.doc),
+        });
+    });
+    proj.forEach((e) => {
+      console.log('name: ' + e.name);
+    });
+    this.ipcService.send('export-project', proj);
   }
 }
